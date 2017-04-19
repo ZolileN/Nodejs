d3088220612057e6c33e30f405bbb1e751b106be
Zolile Nonzapa <zolile@mlkcomputer.co.za>
2017-04-19 19:37:43 +0200

    Views created

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,6 @@
 // require express
 var express = require('express');
+var path    = require('path');
 
 // create our router object
 var router = express.Router();
@@ -9,7 +10,8 @@ module.exports = router;
 
 // route for our homepage
 router.get('/', function(req, res) {
-    res.send('hello world new!');
+   // res.send('hello world new!');
+    res.sendFile(path.join(__dirname, '../index.html'))
 });
 
 //route for our about page
--- server.js
+++ server.js
@@ -5,8 +5,11 @@ var app = express();
 var port = 8080;
 
 // route our app
-var router = require('/app/routes');
+var router = require('./app/routes');
 app.use('/', router);
+
+// set static files (css and images, etc) location
+app.use(express.static(__dirname + '/public'));
 
 // start the server
 app.listen(port, function () {
