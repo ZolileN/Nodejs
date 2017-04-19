2e0cb6f226c62a1af6592cb0991164fa992f8676
Zolile Nonzapa <zolile@mlkcomputer.co.za>
2017-04-19 18:01:46 +0200

    two push

---
+++ server.js
@@ -0,0 +1,24 @@
+
+//require dependencies
+var express = require('express');
+var app = express();
+var port = 8080;
+
+// route our app
+var router = require('/app/routes');
+app.use('/', router);
+
+// start the server
+app.listen(port, function () {
+    console.log('app started');
+});
+
+
+
+
+
+
+
+
+
+
